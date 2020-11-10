953798bd68c300c560baff2f189fe50d57befcf8
lekster07 <60404649+lekster07@users.noreply.github.com>
2020-11-10 21:07:20 +0800

    commit

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      
+    </v-app-bar>
+
+    <v-main>
+      <RequestForm/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import RequestForm from './components/RequestForm';
+
+export default {
+  name: 'App',
+
+  components: {
+    RequestForm,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client/src/components/RequestForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container class="request-form-main">
+        <h1>Request Form</h1>
+        <v-btn dark color="blue" @click="sendEmail()">Email</v-btn>
+    </v-container>
+</template>
+
+<script>
+// const Email  = require('https://smtpjs.com/v3/smtp.js');
+
+export default {
+    name: 'RequestForm',
+
+    methods: {
+        sendEmail() {
+            alert(1);
+            // Email.send({
+            // Host: 'smtp.gmail.com',
+            // Username : 'coeasiaautomation@gmail.com',
+            // Password : "Inchcap3",
+            // To : 'lester.fajardo@gmail.com',
+            // From : 'coeasiaautomation@gmail.com',
+            // Subject : '<email subject>',
+            // Body : '<h1>Hello!</h1>',
+            // }).then(
+            //     message => alert("mail sent successfully")
+            // );
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
